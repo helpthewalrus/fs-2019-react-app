3d9821cb27f7c9736db9aaae2b02434138e8e0d4
Alina Tylets <alinatylets@gmail.com>
2019-12-15 12:36:34 +0300

    feat: create Header component with styles

---
+++ src/components/Header/Header.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+import "./Header.scss";
+import { Logo } from "../Shared/Logo/Logo";
+
+export const Header = () => {
+  return (
+    <div className="header">
+      <Logo />
+    </div>
+  );
+};
