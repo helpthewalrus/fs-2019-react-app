3f9a39457d780ff33d6d635f64b4d461843e0994
Alina Tylets <alinatylets@gmail.com>
2019-12-15 12:30:34 +0300

    feat: create Logo component with styles

---
+++ src/components/Shared/Logo/Logo.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "./Logo.scss";
+
+export const Logo = () => {
+  return (
+    <p className="app-logo">
+      <span className="app-logo__bold">netflix</span>
+      <span className="app-logo__normal">roulette</span>
+    </p>
+  );
+};
