4b44ee8513a8c4987611b142ba40768a5e448823
Alina Tylets <alinatylets@gmail.com>
2019-12-15 12:40:03 +0300

    feat: create MoviesList compoent with styles

---
+++ src/components/MoviesList/MoviesList.js
@@ -0,0 +1,26 @@
+import React from "react";
+import { MovieCard } from "./MovieCard/MovieCard";
+import "./MoviesList.scss";
+
+export const MoviesList = ({ movies, sortType }) => {
+  const sortMovies = movies => {
+    return movies.sort((a, b) => {
+      return a[sortType] < b[sortType] ? -1 : 1;
+    });
+  };
+
+  const resultMovies = movies => {
+    if (movies.length) {
+      return sortMovies(movies).map(movie => {
+        return <MovieCard key={movie.id} {...movie} />;
+      });
+    }
+    return (
+      <li className="no-movies-found">
+        <h2>No movies found</h2>
+      </li>
+    );
+  };
+
+  return <ul className="movies-cards-list">{resultMovies(movies)}</ul>;
+};
